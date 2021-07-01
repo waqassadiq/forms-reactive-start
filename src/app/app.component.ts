7988dc01bb960caa36e7bbc7cf002c243a4f539c
Waqas <wakassadik@gmail.com>
2021-07-01 16:06:15 +0500

    retrive user error codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,14 @@ export class AppComponent implements OnInit{
 
   genders = ['male', 'female'];
   signupForm: FormGroup;
-  
+  forbiddenUsernames = ['Khan','Mahi'];
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
 
       'userData': new FormGroup({
         // Validators.required() don't execute it, just give the reference as below
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -32,5 +32,13 @@ export class AppComponent implements OnInit{
   onAddHobby(){
     const control  = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control)
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean}{
+    if(this.forbiddenUsernames.indexOf(control.value) !== -1){
+      return {'nameIsForbidden': true}
+    }
+    // if validation is false, just return null or simply ommit the return statement, 
+    return null;
   }
 }
